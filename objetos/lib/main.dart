c7fc32b896d56da1b3b142b57f2b0a4bee1db43a
Ricardo Viana <richardluizv@gmail.com>
2019-10-29 00:18:23 -0300

    criando metodos

--- objetos/lib/main.dart
+++ objetos/lib/main.dart
@@ -1,9 +1,21 @@
-import 'package:objetos/src/gato.dart';
+
+import 'dart:io';
+
+import 'package:objetos/src/pessoa2.dart';
+
+Pessoa2 pessoa = Pessoa2();
 
 main(List<String> arguments) {
+  print("--- escreva o seu nome ---");
+  pessoa.nome = stdin.readLineSync();
 
-    Gato gato = Gato(nome: "Tilete", barulho: "Miau");
+  print("--- escreva a sua idade ---");
+  pessoa.idade = int.parse(stdin.readLineSync());
 
-    print("Gato faz: ${gato.barulho}");
+  print("--- escreva a sua altura ---");
+  pessoa.altura = double.parse(stdin.readLineSync());
+
+  print("--- escreva o seu peso ---");
+  pessoa.peso = double.parse(stdin.readLineSync());
   
 }
---
+++ objetos/lib/src/pessoa2.dart
@@ -0,0 +1,8 @@
+class Pessoa2 {
+
+  String nome;
+  int idade;
+  double altura;
+  double peso;
+
+}
