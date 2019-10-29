9c63badde70019a8cb6fa9a46d160c3b176ac4b2
Ricardo Viana <richardluizv@gmail.com>
2019-10-28 23:20:20 -0300

    criando funcionalidade carrinho de compras na estrutura console application

--- estudo/lib/main.dart
+++ estudo/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:estudo/src/calculo_idade.dart';
+import 'package:estudo/src/carrinho_compras.dart';
 
 main(List<String> arguments) {
 
   if (arguments[0] == "calculo-idade") {
     calcular();
+  } else if (arguments[0] == "carrinho-compras") {
+    carrinhoCompras();
   } else {
-    print("Este programa nao existe");
+    print("<=============> ESTE PROGRAMA NAO EXISTE <=============>");
   }
   
 }
